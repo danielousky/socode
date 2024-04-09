3f704426405de0ef2e49c9641024094431e592cc
Daniel Moros <danielousky@gmail.com>
2024-04-09 12:25:19 -0400

    Added haml and initial bootstrap

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -1,3 +1,16 @@
 // Configure your import map in config/importmap.rb. Read more: https://github.com/rails/importmap-rails
 import "@hotwired/turbo-rails"
 import "controllers"
+import "popper"
+import "bootstrap"
+
+document.addEventListener('DOMContentLoaded', function() {
+  let popoverTriggerList = [].slice.call(document.querySelectorAll('[data-bs-toggle="popover"]'))  
+  let popoverList = popoverTriggerList.map(function (popoverTriggerEl) {  
+    return new bootstrap.Popover(popoverTriggerEl)  
+  })  
+
+  const tooltipTriggerList = document.querySelectorAll('[data-bs-toggle="tooltip"]')
+  const tooltipList = [...tooltipTriggerList].map(tooltipTriggerEl => new bootstrap.Tooltip(tooltipTriggerEl))
+
+});
